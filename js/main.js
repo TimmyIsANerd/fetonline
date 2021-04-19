70cc16eb10639dc28983a22d82e7da0415ce895f
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2021-04-19 16:09:34 +0100

    Got started with fetonline.com.ng

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// Toggle Hamburger Button
+
+var nav = document.querySelector('#setTopNav');
+var ham = document.querySelector('#ham');
+var icon = document.querySelector('.fa');
+var bar = document.querySelector('.vert-bar');
+
+const toggleHam = () =>{
+    if(nav.className === 'topnav'){
+        nav.className += ' responsive';
+    }else{
+        nav.className = "topnav";
+    }
+}
+
+ham.addEventListener('click',()=>{
+    console.log('Click Event Occured');
+    if(icon.className === 'fa fa-bars'){
+        icon.classList.remove('fa-bars');
+        icon.classList.add('fa-times');
+        bar.style.display = 'block';
+    } else {
+        icon.classList.remove('fa-times')
+        icon.classList.add('fa-bars');
+        bar.style.display = 'none';
+    }
+});
