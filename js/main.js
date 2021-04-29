6d3b343553071420a896057cca7b73739fcbade3
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2021-04-29 18:53:04 +0100

    wrote toggle feature for faq

--- js/main.js
+++ js/main.js
@@ -30,3 +30,30 @@ var date = document.querySelector('#date');
 var year = new Date();
 console.log(year)
 date.textContent = `${year.getFullYear()}`;
+
+// Toggle Feature
+var arrowIcon = document.querySelectorAll('.arrow');
+console.log(arrowIcon);
+
+
+// const arrowToggle = (icon) =>{
+//     if(icon[i].className === 'fa fa-chevron-up'){
+//         icon[i].classList.remove('fa fa-chevron-up');
+//         icon[i].classList.add('fa-chevron-down');
+//     } else {
+//         icon[i].classList.remove('fa fa-chevron-up');
+//         icon[i].classList.add('fa-chevron-down');
+//     }
+// }
+
+arrowIcon.forEach(function(icon){
+    icon.addEventListener('click',()=>{
+        if(icon.className === 'fa fa-chevron-up'){
+            icon.classList.remove('fa fa-chevron-up');
+            icon.classList.add('fa-chevron-down');
+        } else {
+            icon.classList.remove('fa fa-chevron-up');
+            icon.classList.add('fa-chevron-down');
+        }
+    })
+});
